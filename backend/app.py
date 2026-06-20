894fd5d827b9dceb93ac77134995a8334e6a4d29
nakashima_yuki's fan <3464609926@qq.com>
2026-06-21 01:17:39 +0800

    关于app.py的部分修改

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, request, jsonify
+from flask import send_from_directory
 from flask_cors import CORS
 from algorithms.scheduler import scheduler_bp
 from algorithms.producer_consumer import producer_consumer_bp
@@ -6,9 +8,32 @@ from algorithms.banker import banker_bp
 from algorithms.page_replace import page_bp
 from algorithms.disk_schedule import disk_bp
 
-app = Flask(__name__)
 #CORS(app)
+app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
+
+BASE_DIR=os.path.dirname(os.path.abspath(__file__))
+FRONTEND_DIR=os.path.join(BASE_DIR,"../frontend")
+#前端托管，可以直接通过ipv4地址访问该网站页面，也可以继续点击index.html
+@app.route("/")
+def home():
+    return send_from_directory(FRONTEND_DIR,"index.html")
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(FRONTEND_DIR,path)
+
+@app.route("/")
+def index():
+    return {
+        "project":"OS Web Simulator",
+        "status":"running"
+    }
+
+
+
+
 
 app.register_blueprint(scheduler_bp)
 app.register_blueprint(producer_consumer_bp)
